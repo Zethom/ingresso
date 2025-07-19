1730b21c801beed81b45dea7d496bed7e3d78894
Matheus G. Alves <matheusga.bean@gmail.com>
2025-07-19 16:48:36 -0300

    Código Prontinho

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function comprar(){
+
+    let tipoDeIngresso = document.getElementById(`tipo-ingresso`).value;
+    let quantidade = document.getElementById(`qtd`).value;
+    let numeroDeIngresso = document.getElementById(`qtd-${tipoDeIngresso}`).textContent;
+
+    console.log(`Ingresso: ${tipoDeIngresso} Quantidade: ${quantidade} Quantidade de Ingressos: ${numeroDeIngresso}`);
+
+    if((numeroDeIngresso - quantidade) >= 0){
+
+        numeroDeIngresso = numeroDeIngresso - quantidade;
+        document.getElementById(`qtd-${tipoDeIngresso}`).textContent = `${numeroDeIngresso}`;
+
+    } else {
+
+        alert(`Não foi possível realizar a compra do ingresso: Quantidade disponível insuficiente`);
+
+    }
+
+}
