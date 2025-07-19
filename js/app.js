9caf9ec172d8ff35668276b0e8f2259112e86e54
Matheus G. Alves <matheusga.bean@gmail.com>
2025-07-19 17:18:29 -0300

    Refinamento do Código; Impedimento de números negativos e arredondamento de strings

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 function comprar(){
 
     let tipoDeIngresso = document.getElementById(`tipo-ingresso`).value;
-    let quantidade = document.getElementById(`qtd`).value;
+    let quantidade = parseInt(document.getElementById(`qtd`).value);
     let numeroDeIngresso = document.getElementById(`qtd-${tipoDeIngresso}`).textContent;
 
     console.log(`Ingresso: ${tipoDeIngresso} Quantidade: ${quantidade} Quantidade de Ingressos: ${numeroDeIngresso}`);
 
-    if((numeroDeIngresso - quantidade) >= 0){
+    if((numeroDeIngresso - quantidade) >= 0 && quantidade > 0){
 
         numeroDeIngresso = numeroDeIngresso - quantidade;
         document.getElementById(`qtd-${tipoDeIngresso}`).textContent = `${numeroDeIngresso}`;
+
+    } else if (quantidade < 0) {
+
+        alert(`Valor inserido inválido`);
 
     } else {
 
